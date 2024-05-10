777f343843780eb0f5ed5d6c746713bb0a00ec52
veenacc <veena.chenicheri@gmail.com>
2024-05-10 14:33:33 -0400

    get fortune and lotto

---
+++ app/controllers/my_examples_controller.rb
@@ -0,0 +1,21 @@
+class MyExamplesController < ApplicationController
+  def seefortune
+    n = rand(1..4)
+    if n == 1
+      render json: {message: "Read daily"}
+    elsif n ==2
+      render json: {message: "Be kind"}
+    else
+      render json: {message: "Smile please"}
+    end
+  end
+  
+  def lotto
+    num =[]
+    6.times do
+      n= rand(1..60)
+      num << n
+    end
+    render json: {message: "The winning numbers are #{num}"}
+  end
+end
